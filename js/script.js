7130ca9218730075ff0ea9d1f07a06f9285e6e83
Vander1221 <fogoraww@mail.ru>
2024-06-25 15:21:48 +0300

    day 2

---
+++ js/script.js
@@ -0,0 +1,186 @@
+// let userName; // Обьявляем переменную
+// console.log(typeof userName);
+// userName = "Фрилансер по жизни"; //Строка (str)
+// console.log(typeof userName);
+// userName = 58; //Число (Num)
+// console.log(typeof userName);
+
+
+// let userName; //Обьявляем переменную
+// //Получаем тип данных
+// console.log(typeof userName);
+// //Получаем значение переменной
+// console.log(userName);
+
+
+
+// Undefiend - неопределенный тип 
+
+// let userName; 
+// console.log(typeof userName);
+// console.log(userName);
+
+// // Примеры использования
+// // Проверяем опредена ли переменная
+
+// if (userName === undefined) {
+//    console.log('Переменная НЕ определенна');
+// }  else {
+//    console.log('Переменная определенна');
+// }
+// // либо с помощью оператора typeof
+
+// if (typeof userName === 'undefined') {
+//    console.log('Переменная НЕ определена');
+// }  else {
+//    console.log('Переменная определена');
+// }
+
+
+
+// // Null
+// let userName = null;
+// console.log(userName);
+
+
+// Пример
+// Попытка обратится к обьекту которого нет
+
+// let block = document.querySelector('.block');
+
+// // Получаем null
+// console.log(block);
+
+// //Вернем тип object
+// console.log(typeof block);
+
+
+// Boolean
+
+
+// Пример
+
+// let willYouMarryMe = true;
+// if (willYouMarryMe) {
+//     console.log(':)');
+// }   else {
+//     console.log(':(');
+// }
+
+
+
+// let trueOrFalse = 58 < 18;
+// console.log(trueOrFalse);
+
+
+// Num
+
+// let userAge = 20;
+// let userHeight = 1.83;
+// console.log(userAge);
+// console.log(typeof userAge);
+// console.log(userHeight);
+// console.log(typeof userHeight);
+
+
+// Infinity
+
+// let getInfinity = 58 / 0;
+// console.log(getInfinity);
+// console.log(typeof getInfinity);
+
+// NaN
+
+// let getNaN = 'Фрилансер' / 10;
+// console.log(getNaN);
+// console.log(typeof getNaN);
+
+
+// BigInt
+
+// const bidInteger = 12345624452445255445453534535363n;
+// console.log(typeof bidInteger);
+
+
+// let userName = "Фрилансер по жизни";
+// console.log(userName);
+
+
+// let userAge = 36;
+// let userAgeInfo = 'Возраст: ${userAge}';
+// console.log(userAgeInfo);
+
+
+// Object
+
+// let userInfo = {
+//     name: "Фрилансер по жизни",
+//     age: 36
+// }
+// console.log(userInfo);
+// console.log(typeof userInfo);
+
+
+// Symbol
+
+// let id = Symbol("id");
+// console.log(typeof id);
+
+
+
+// function
+
+
+// let funcVariable = function name(params) {
+//     // код функции
+// }
+// console.log(typeof funcVariable);
+
+
+// Преобразование типов
+
+// let userAge = 58;
+// console.log(userAge);
+// console.log(typeof userAge);
+
+// userAge = String(userAge);
+
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+// let userAge = "58";
+// console.log(userAge);
+// console.log(typeof userAge);
+
+// userAge = Number(userAge);
+
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+// let userAge = "72" / "2";
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+
+// Логическое преобразование
+
+// let userAge = 44;
+// console.log(userAge);
+// console.log(typeof userAge);
+
+// userAge = Boolean(userAge);
+
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+
+
+
+// Homework
+
+// let userName;
+// console.log(typeof userName);
